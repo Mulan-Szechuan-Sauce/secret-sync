4b4eaf0d377c5297425ebe7a768d25c0556a3040
Josh Wolfe <joshuafwolfe@gmail.com>
2025-03-15 01:15:26 -0400

    Properly handle updating CRDs without reprocessing everything

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
-use std::{collections::HashMap, sync::Arc, time::Duration};
+use std::sync::Arc;
 
 use clap::Parser;
-use futures::{StreamExt, TryStreamExt, stream};
+use futures::{StreamExt, TryStreamExt};
 use k8s_openapi::api::core::v1::Secret;
 use kube::{
-    Api, Client, CustomResourceExt, ResourceExt,
-    api::ListParams,
-    config::KubeConfigOptions,
-    runtime::{WatchStreamExt, reflector, watcher},
+    config::KubeConfigOptions, runtime::{reflector, watcher, WatchStreamExt}, Api, Client, CustomResourceExt, ResourceExt
 };
 use kube_derive::CustomResource;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use tokio::sync::Mutex;
+use tokio::sync::Notify;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -28,8 +25,8 @@ struct SecretTarget {
 }
 
 #[derive(CustomResource, Clone, Debug, Deserialize, Serialize, JsonSchema)]
-#[kube(group = "homerow.ca", version = "v1", kind = "Foo", namespaced)]
-struct FooSpec {
+#[kube(group = "homerow.ca", version = "v1", kind = "SyncSecret", namespaced)]
+struct SyncSecretSpec {
     secret: SecretTarget,
 }
 
@@ -42,43 +39,48 @@ async fn run() -> anyhow::Result<()> {
     let config = kube::Config::from_kubeconfig(&options).await?;
     let client = Client::try_from(config)?;
 
-    let (reader, writer) = reflector::store::<Foo>();
+    let (reader, writer) = reflector::store::<SyncSecret>();
+
+    let new_crd_notify = Arc::new(Notify::new());
+    let new_crd_awaiter = new_crd_notify.clone();
 
     let secret_api = Api::<Secret>::all(client.clone());
     tokio::spawn(async move {
         reader.wait_until_ready().await.unwrap();
 
-        let mut secret_watcher = watcher(secret_api, watcher::Config::default())
-            .applied_objects()
-            .default_backoff()
-            .boxed();
+        loop {
+            let mut secret_watcher = watcher(secret_api.clone(), watcher::Config::default())
+                .applied_objects()
+                .default_backoff()
+                .boxed();
 
-        while let Some(secret) = secret_watcher
-            .try_next()
-            .await
-            .expect("Unable to read from secret stream")
-        {
-            for target in reader.state().iter() {
-                process_match(&target, &secret).await;
+            loop {
+                tokio::select! {
+                    _ = new_crd_awaiter.notified() => break,
+
+                    Ok(Some(secret)) = secret_watcher.try_next() => {
+                        for target in reader.state().iter() {
+                            process_match(&target, &secret).await;
+                        }
+                    }
+                }
             }
         }
     });
 
-    let crd_api = Api::<Foo>::all(client.clone());
+    let crd_api = Api::<SyncSecret>::all(client.clone());
     watcher(crd_api, watcher::Config::default())
         .reflect(writer)
         .applied_objects()
         .default_backoff()
         .boxed()
         .for_each(|res| {
-            let api = Api::<Secret>::all(client.clone());
+            let notify = new_crd_notify.clone();
             async move {
                 match res {
                     Ok(o) => {
                         println!("Found: {}", o.name_any());
-                        for secret in api.list(&ListParams::default()).await.expect("Failed to list secrets") {
-                            process_match(&o, &secret).await;
-                        }
+                        notify.notify_waiters();
                     }
                     Err(e) => println!("watcher error: {}", e),
                 }
@@ -89,7 +91,7 @@ async fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn process_match(target: &Foo, secret: &Secret) {
+async fn process_match(target: &SyncSecret, secret: &Secret) {
     if secret.name_any() == target.spec.secret.name
         && secret.namespace().expect("Secrets should have namespaces")
             == target.spec.secret.namespace
@@ -108,7 +110,7 @@ async fn main() -> anyhow::Result<()> {
 
     match args {
         Args::Crds => {
-            println!("{}", serde_yaml::to_string(&Foo::crd()).unwrap());
+            println!("{}", serde_yaml::to_string(&SyncSecret::crd()).unwrap());
         }
         Args::Run => {
             run().await?;
