b65029ea32171936cc9b056a81f2adfbd927ef96
Josh Wolfe <joshuafwolfe@gmail.com>
2025-03-18 16:37:09 -0400

    Also test that deleting properly deletes owned replicated secrets

--- tests/k3s_integration.rs
+++ tests/k3s_integration.rs
@@ -1,19 +1,14 @@
-use std::error::Error;
-
 use k8s_openapi::{
     api::core::v1::{Namespace, Secret},
     apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition,
 };
 use kube::{
-    Client, CustomResourceExt, ResourceExt,
+    Api, Client, CustomResourceExt, ResourceExt,
     api::{DeleteParams, ObjectMeta, Patch, PatchParams},
     config::KubeConfigOptions,
 };
 use secret_sync::crds::*;
-use tokio::{
-    sync::OnceCell,
-    time::{Duration, sleep},
-};
+use tokio::sync::OnceCell;
 use tokio_retry2::{Retry, RetryError, strategy::ExponentialBackoff};
 
 static ONCE_CLIENT: OnceCell<anyhow::Result<Client>> = OnceCell::const_new();
@@ -111,36 +106,71 @@ async fn secrets_replicate() {
         .map(tokio_retry2::strategy::jitter)
         .take(3);
 
-    let result = Retry::spawn(
-        retry_strategy,
+    Retry::spawn(
+        retry_strategy.clone(),
         async || -> Result<(), RetryError<()>> {
             for target in syncer.spec.destination_namespaces.iter() {
-                let secret_api = kube::Api::<Secret>::namespaced(client.clone(), target);
+                let secret_api = Api::<Secret>::namespaced(client.clone(), target);
 
-                match secret_api.get(&syncer.spec.secret.name).await {
-                    Ok(s) => {
-                        assert_eq!(s.data, secret.data);
-                        assert_eq!(s.string_data, secret.string_data);
-                    }
-                    Err(kube::Error::Api(e)) if e.code == 404 => {
-                        return Err(RetryError::transient(()));
-                    }
-                    Err(e) => panic!("Unable to get secret: {}", e),
-                }
+                validate_secret_replicated(&secret_api, &secret).await?;
             }
 
             Ok(())
         },
     )
-    .await;
+    .await
+    .expect("Failure replicating secrets");
 
-    if result.is_err() {
-        panic!("Failure replicating secrets after retries");
+    delete_syncer(client, &syncer.name_any()).await;
+
+    Retry::spawn(
+        retry_strategy.clone(),
+        async || -> Result<(), RetryError<()>> {
+            for target in syncer.spec.destination_namespaces.iter() {
+                let secret_api = Api::<Secret>::namespaced(client.clone(), target);
+
+                validate_secret_deleted(&secret_api, &secret).await?;
+            }
+
+            Ok(())
+        },
+    )
+    .await
+    .expect("Owned secrets not successfully deleted");
+}
+
+async fn delete_syncer(client: &Client, name: &str) {
+    let sync_api = kube::Api::<SyncSecret>::all(client.clone());
+    sync_api
+        .delete(name, &DeleteParams::foreground())
+        .await
+        .expect("Unable to delete SyncSecret");
+}
+
+async fn validate_secret_replicated(
+    secret_api: &Api<Secret>,
+    secret: &Secret,
+) -> Result<(), RetryError<()>> {
+    match secret_api.get(&secret.name_any()).await {
+        Ok(s) => {
+            assert_eq!(s.data, secret.data);
+            assert_eq!(s.string_data, secret.string_data);
+        }
+        Err(kube::Error::Api(e)) if e.code == 404 => {
+            return Err(RetryError::transient(()));
+        }
+        Err(e) => panic!("Unable to get secret: {}", e),
     }
+    Ok(())
+}
 
-    // let sync_api = kube::Api::<SyncSecret>::all(client.clone());
-    // sync_api
-    //     .delete(&syncer.name_any(), &DeleteParams::foreground())
-    //     .await
-    //     .expect("Unable to delete SyncSecret");
+async fn validate_secret_deleted(
+    secret_api: &Api<Secret>,
+    secret: &Secret,
+) -> Result<(), RetryError<()>> {
+    match secret_api.get(&secret.name_any()).await {
+        Ok(_) => Err(RetryError::transient(())),
+        Err(kube::Error::Api(e)) if e.code == 404 => Ok(()),
+        Err(e) => panic!("Unable to get secret: {}", e),
+    }
 }
